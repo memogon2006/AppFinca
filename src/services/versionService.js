611dab3dcd505f1cdabce608281b01497d2078a5
Luis Gonzalez <criaderosantateresa21@gmail.com>
2026-09-06 08:57:21 -0500

    chore: Restaurar aplicacion a version previa (v2.8.3)

--- src/services/versionService.js
+++ src/services/versionService.js
@@ -1,5 +1,5 @@
 export const CURRENT_APP_VERSION = "2.8.3";
-export const CURRENT_BUILD_TIME = 1772950000000;
+export const CURRENT_BUILD_TIME = 1788900000000;
 
 /**
  * Consulta en la nube si hay una nueva versión publicada
